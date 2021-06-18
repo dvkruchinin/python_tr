6eb8e3b5b92629150aa1e02601c3ed1e76ca4cf0
dvkruchinin <dvkruchinin@gmail.com>
2021-06-18 16:20:33 +0300

    Added test for contact creation

---
+++ contact.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Name    : contact.py
+Author  : Dmitry Kruchinin
+Date    : 6/18/2021
+Desc:   
+"""
+
+class Contact:
+
+    def __init__(self, first_name, last_name, address, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.phone = phone
+        self.email = email
