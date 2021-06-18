70336ddb1bdf0088df134dd1e72b9db05f14f6b7
dvkruchinin <dvkruchinin@gmail.com>
2021-06-18 15:11:41 +0300

    Auxiliary methods and classes

---
+++ group.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Name    : group.py
+Author  : Dmitry Kruchinin
+Date    : 6/18/2021
+Desc:   
+"""
+
+class Group:
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
