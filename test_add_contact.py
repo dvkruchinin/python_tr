d0e0cb2c4608db03604f274e703adb77924289e5
dvkruchinin <dvkruchinin@gmail.com>
2021-06-21 11:40:04 +0300

    Refactoring.

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(first_name="fname",
-                                        last_name="lname",
-                                        address="home",
-                                        phone="9999999999",
-                                        email="name@home.local"))
-        self.app.return_to_home_page()
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(first_name="fname",
+                               last_name="lname",
+                               address="home",
+                               phone="9999999999",
+                               email="name@home.local"))
+    app.return_to_home_page()
+    app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="qwe", header="qwe", footer="qwe"))
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="qwe", header="qwe", footer="qwe"))
+    app.logout()
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
