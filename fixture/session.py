d67d0b97cc8df31c53fc1f013e6760afe9e7ba06
dvkruchinin <dvkruchinin@gmail.com>
2021-06-24 12:49:14 +0300

    Little bit update is_logged_in_as()

--- fixture/session.py
+++ fixture/session.py
@@ -38,7 +38,7 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//*[@id='top']/form/b").text == "("+username+")"
+        return wd.find_element_by_xpath("//*[@id='top']/form/b").text == "(%s)" % username
 
     def ensure_login(self, username, password):
         if self.is_logged_in():
