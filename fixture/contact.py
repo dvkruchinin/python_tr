e6ba7f47f9b2d880eb7d64c6cfb4e8c30e50ef77
dvkruchinin <dvkruchinin@gmail.com>
2021-06-28 13:11:37 +0300

    Updated test for a phone checking.

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,6 +8,7 @@ Desc:
 """
 
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -129,4 +130,15 @@ class ContactHelper:
         workphone = wd.find_element_by_name("work").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, mobilephone=mobilephone,
+                       workphone=workphone, secondaryphone=secondaryphone)
+
+    def get_contact_info_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, mobilephone=mobilephone,
                        workphone=workphone, secondaryphone=secondaryphone)
