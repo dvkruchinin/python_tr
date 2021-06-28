774f2465658570a3237c7c15a7efb6119a8fff9c
dvkruchinin <dvkruchinin@gmail.com>
2021-06-28 22:09:04 +0300

    Parameterization of tests for creating groups and contacts

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone():
+    digits = string.digits + " "*10
+    return "".join([random.choice(digits) for i in range(10)])
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 5), lastname=random_string("lastname", 5),
+                homephone=random_phone(), mobilephone=random_phone(),
+                workphone=random_phone(), secondaryphone=random_phone(), address=random_string("address", 5),
+                email=random_string("email", 5), email2=random_string("email2", 5), email3=random_string("email3", 5))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="firstname", lastname="lastname",
-                                homephone="homephone111", mobilephone="mobilephone222",
-                                workphone="workphone333", secondaryphone="secphone444", address="home",
-                                email="name@home.local", email2="name2@home.local", email3="name3@home.local")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
