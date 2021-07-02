7ea612d801e9f18ab71fe57981b2c23f406593cd
dvkruchinin <dvkruchinin@gmail.com>
2021-07-02 12:58:25 +0300

    Added check_ui

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -11,7 +11,7 @@ import random
 from model.contact import Contact
 
 
-def test_delete_some_contact(app, db):
+def test_delete_some_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.group.create(Contact(firstname="firstname", lastname="lastname",
                                 homephone="homephone111", mobilephone="mobilephone222",
@@ -23,3 +23,5 @@ def test_delete_some_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
